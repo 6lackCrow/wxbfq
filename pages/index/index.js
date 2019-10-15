fb514a558af7c81f0cd57b6a18abc8873a18ed2d
crow <1871339825@qq.com>
2019-10-15 11:37:59 +0800

    init

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,9 +4,40 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    item:0,
+    tab:0,
+    playlist:[
+      { id: 1, title: 'Ransom', singer: 'Lil Tecca',src:'',coverImgUrl:'/images/cover.jpg'},
+      { id: 2, title: 'Empty', singer: 'Juice Wrld',src:'',coverImgUrl:'/images/cover.jpg'},
+      { id: 3, title: 'Nowadays', singer: 'Lil Skies',src:'',coverImgUrl:'/images/cover.jpg'},
+      { id: 4, title: 'GOTTI', singer: '6ix9ine',src:'',coverImgUrl:'/images/cover.jpg'},
+    ],
+    state:'paused',
+    playIndex:0,
+    play:{
+      currentTime:'00:00',
+      duration:'00:00',
+      percent:0,
+      title:'',
+      singer:'',
+      coverImgUrl:'/images/cover.jpg',
+    }
   },
 
+
+  changeItem: function (e) {
+    this.setData({
+      item:e.target.dataset.item,
+    })
+  },
+
+  changeTab: function (e) {
+    this.setData({
+      tab:e.detail.current
+    })
+  },
+
+  
   /**
    * 生命周期函数--监听页面加载
    */
@@ -17,9 +48,26 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
+  audioCtx:null,
   onReady: function () {
-    
+    this.audioCtx = wx.createInnerAudioContext()
+    this.setMusic(0)
   },
+
+  setMusic: function (index) {
+    var music = this.data.playlist[index]
+    this.audioCtx.src = music.src
+    this.setData({
+      playIndex: index,
+      'play.title': music.title,
+      'play.singer': music.singer,
+      'play.coverImgUrl': music.coverImgUrl,
+      'play.currentTime': '00:00',
+      'play.duration': '00:00',
+      'play.percent': 0
+    })
+  },
+
 
   /**
    * 生命周期函数--监听页面显示
